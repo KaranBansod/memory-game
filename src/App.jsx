d0a8dd0787e9e0d6041fbe6ac8333c968b0982f5
KaranBansod <karanbansod79@gmail.com>
2024-12-25 15:17:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Memorygame from './components/memory-game'
+
+function App() {
+  return (
+    <div>
+    <Memorygame />
+      </div>
+  )
+}
+
+export default App
