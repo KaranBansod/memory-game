87baac7a753e7107fb1409e22c58e0cb3a95209e
KaranBansod <153828974+KaranBansod@users.noreply.github.com>
2024-12-25 23:56:28 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MemoryGame from './components/Memory-game.jsx'
+
+
+
+function App() {
+  return (
+    <div>
+     <MemoryGame />
+      </div>
+  )
+}
+
+export default App
